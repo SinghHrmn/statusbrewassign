54f9835c298ab2c340669e7b2f37d6c2ca33b206
Harmandeep Singh <singh.hrmn98@gmail.com>
2024-05-22 23:49:52 +0530

    tests: added tests for color grid select, color grid item component

--- libs/ng/ui/components/src/lib/color-grid-select/color-grid-select.component.stories.ts
+++ libs/ng/ui/components/src/lib/color-grid-select/color-grid-select.component.stories.ts
@@ -1,9 +1,38 @@
 import type { Meta, StoryObj } from '@storybook/angular';
 import { ColorGridSelectComponent } from './color-grid-select.component';
 
-import { within } from '@storybook/testing-library';
+import { userEvent, within } from '@storybook/testing-library';
 import { expect } from '@storybook/jest';
 import { COLOR_GRID_ITEMS, COLOR_GRID_ITEM_SIZES } from './item';
+
+const KEY_OPTIONS = {
+  bubbles: true,
+  cancelable: true,
+};
+
+const arrowUpKeyboardEvent = new KeyboardEvent('keydown', {
+  ...KEY_OPTIONS,
+  keyCode: 38,
+  key: 'ArrowUp',
+});
+
+const arrowDownKeyboardEvent = new KeyboardEvent('keydown', {
+  ...KEY_OPTIONS,
+  keyCode: 40,
+  key: 'ArrowDown',
+});
+
+const arrowLeftKeyboardEvent = new KeyboardEvent('keydown', {
+  ...KEY_OPTIONS,
+  keyCode: 37,
+  key: 'ArrowLeft',
+});
+
+const arrowRightKeyboardEvent = new KeyboardEvent('keydown', {
+  ...KEY_OPTIONS,
+  keyCode: 39,
+  key: 'ArrowRight',
+});
 
 const meta: Meta<ColorGridSelectComponent> = {
   component: ColorGridSelectComponent,
@@ -43,5 +72,90 @@ export default meta;
 type Story = StoryObj<ColorGridSelectComponent>;
 
 export const Primary: Story = {
-  args: {},
+  name: 'Small Mobile',
+  args: {
+    items: [
+      '#FF5733', '#FFBD33', '#DBFF33', '#75FF33', '#33FF57',
+      '#33FFBD', '#33DBFF', '#3375FF', '#5733FF', '#BD33FF',
+      '#FF33DB', '#FF3375'
+    ],
+    itemSize: 'medium',
+    disabled: false
+  },
+  parameters: {
+    viewport: { defaultViewport: 'mobile1' },
+  },
+  play: async ({canvasElement}) => {
+    const canvas = within(canvasElement);
+
+    // Find the first color item by its aria-label
+    const firstItem = await canvas.findByRole('option', { name: '#FF5733' });
+
+    // Focus on the first item
+    await userEvent.click(firstItem);
+    expect(firstItem).toHaveFocus();
+
+    // Navigate right
+    firstItem.dispatchEvent(arrowRightKeyboardEvent)
+    const secondItem = await canvas.findByRole('option', { name: '#FFBD33' });
+    expect(secondItem).toHaveFocus();
+
+    // Navigate down
+    await secondItem.dispatchEvent(arrowDownKeyboardEvent);
+    const seventhItem = await canvas.findByRole('option', { name: '#33FFBD' });
+    expect(seventhItem).toHaveFocus();
+
+    // Navigate up
+    seventhItem.dispatchEvent(arrowUpKeyboardEvent);
+    expect(secondItem).toHaveFocus();
+
+    // Navigate left
+    secondItem.dispatchEvent(arrowLeftKeyboardEvent);
+    expect(firstItem).toHaveFocus();
+  },
+};
+
+
+export const Secondary: Story = {
+  name: 'Large Mobile',
+  args: {
+    items: [
+      '#FF5733', '#FFBD33', '#DBFF33', '#75FF33', '#33FF57',
+      '#33FFBD', '#33DBFF', '#3375FF', '#5733FF', '#BD33FF',
+      '#FF33DB', '#FF3375'
+    ],
+    itemSize: 'medium',
+    disabled: false
+  },
+  parameters: {
+    viewport: { defaultViewport: 'mobile2' },
+  },
+  play: async ({canvasElement}) => {
+    const canvas = within(canvasElement);
+
+    // Find the first color item by its aria-label
+    const firstItem = await canvas.findByRole('option', { name: '#FF5733' });
+
+    // Focus on the first item
+    await userEvent.click(firstItem);
+    expect(firstItem).toHaveFocus();
+
+    // Navigate right
+    firstItem.dispatchEvent(arrowRightKeyboardEvent)
+    const secondItem = await canvas.findByRole('option', { name: '#FFBD33' });
+    expect(secondItem).toHaveFocus();
+
+    // Navigate down
+    await secondItem.dispatchEvent(arrowDownKeyboardEvent);
+    const seventhItem = await canvas.findByRole('option', { name: '#33DBFF' });
+    expect(seventhItem).toHaveFocus();
+
+    // Navigate up
+    seventhItem.dispatchEvent(arrowUpKeyboardEvent);
+    expect(secondItem).toHaveFocus();
+
+    // Navigate left
+    secondItem.dispatchEvent(arrowLeftKeyboardEvent);
+    expect(firstItem).toHaveFocus();
+  },
 };
--- libs/ng/ui/components/src/lib/color-grid-select/item/item.component.stories.ts
+++ libs/ng/ui/components/src/lib/color-grid-select/item/item.component.stories.ts
@@ -5,7 +5,7 @@ import {
   ColorGridItemComponent,
 } from './item.component';
 
-import { within } from '@storybook/testing-library';
+import { userEvent, within } from '@storybook/testing-library';
 import { expect } from '@storybook/jest';
 
 const meta: Meta<ColorGridItemComponent> = {
@@ -16,6 +16,7 @@ export default meta;
 type Story = StoryObj<ColorGridItemComponent>;
 
 export const Primary: Story = {
+  name: 'Selectable',
   args: {
     value: COLOR_GRID_ITEMS[0],
     size: COLOR_GRID_ITEM_SIZES[0],
@@ -31,4 +32,38 @@ export const Primary: Story = {
       options: COLOR_GRID_ITEM_SIZES,
     },
   },
+  play: async ({canvasElement}) => {
+    const canvas = within(canvasElement);
+
+    const item = await canvas.findByRole('option')
+    await userEvent.click(item);
+    expect(item).toHaveAttribute('aria-selected', "true");
+  }
+};
+
+export const Secondary: Story = {
+  name: 'Disabled Item',
+  args: {
+    value: COLOR_GRID_ITEMS[0],
+    size: COLOR_GRID_ITEM_SIZES[0],
+    checked: false,
+    disabled: true
+  },
+  argTypes: {
+    value: {
+      control: 'select',
+      options: COLOR_GRID_ITEMS,
+    },
+    size: {
+      control: 'radio',
+      options: COLOR_GRID_ITEM_SIZES,
+    },
+  },
+  play: async ({canvasElement}) => {
+    const canvas = within(canvasElement);
+
+    const item = await canvas.findByRole('option')
+    await userEvent.click(item);
+    expect(item).toHaveAttribute('aria-selected', "false");
+  }
 };
